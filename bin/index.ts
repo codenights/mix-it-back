58e1dfa353c59604c3b0f813d5e4e988e4977f15
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-31 19:32:45 +0000

    Deploy codenights/mix-it-back to github.com/codenights/mix-it-back.git:gh-pages

--- bin/index.ts
+++
@@ -1,5 +0,0 @@
-import createApp from '../src/app'
-import logger from '../src/core/logger'
-
-const app = createApp()
-app.start().catch(logger.error)
--- src/party/party.ts
+++
@@ -1,6 +0,0 @@
-import { Playlist } from './playlist'
-
-export interface Party {
-  id?: string
-  playlist: Playlist
-}
